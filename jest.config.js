5af238369440f348b8ebfe06b36898009df7a694
Felix Kästner <kaestner.fe@gmail.com>
2020-11-14 18:01:18 +0100

    feat: add __DEV__ global to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/types/'],
+  globals: {
+    __DEV__: true,
+  },
   moduleNameMapper: {
     '^@/(.*)': '<rootDir>/src/$1',
   },
