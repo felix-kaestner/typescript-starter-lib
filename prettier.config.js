9c368ed07404dc7deba1472a1a07bbb41318cb85
Felix Kästner <mail@felix-kaestner.com>
2022-01-30 17:05:18 +0100

    chore: switch to json config

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'es5',
-  semi: false,
-  printWidth: 80,
-  importOrder: ['<THIRD_PARTY_MODULES>', '^@/(.*)$', '^[~/]', '^[./]'],
-  importOrderSortSpecifiers: true,
-}
