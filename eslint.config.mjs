e094b9b05ce92dd85f1e0947cf8aecd0d98b6aca
Felix Kästner <felix.kaestner@aboutyou.com>
2024-12-25 22:32:10 +0100

    chore: update lint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,25 @@
-import {FlatCompat} from '@eslint/eslintrc'
+// @ts-check
 import js from '@eslint/js'
-import typescript from '@typescript-eslint/eslint-plugin'
-import parser from '@typescript-eslint/parser'
+import prettier from 'eslint-plugin-prettier/recommended'
 import globals from 'globals'
-import path from 'node:path'
-import {fileURLToPath} from 'node:url'
+import ts from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
-
-export default [
+export default ts.config(
+  {ignores: ['coverage/**', 'dist/**', 'types/**']},
+  js.configs.recommended,
+  ts.configs.recommended,
+  prettier,
   {
-    ignores: ['**/node_modules/*', '**/coverage/*', '**/dist/*', '**/types/*'],
-  },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': typescript,
-    },
-
+    files: ['**/*.{js,mjs,ts}'],
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.browser,
+        ...globals.es2021,
       },
 
-      parser: parser,
       ecmaVersion: 2021,
       sourceType: 'module',
     },
-  },
-]
+  }
+)
